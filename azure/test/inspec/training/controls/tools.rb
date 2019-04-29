efbc6cdabb9decc7e6f7128416cf03457f248169
Sean Carolan <scarolan@hashicorp.com>
2019-04-29 08:08:51 -0500

    Attempt to separate setup.ps1 from other tests.

--- azure/test/inspec/training/controls/tools.rb
+++ azure/test/inspec/training/controls/tools.rb
@@ -1,5 +1,6 @@
 # Makes sure all required tools are installed
 
+# https://hashicorp.github.io/se-terraform-vault-workshop/azure/terraform/#42
 control 'terraform-version' do
   impact 1.0
   desc 'Checks to see that Terraform is installed and working.'
@@ -29,5 +30,30 @@ control 'vsc-version' do
   desc 'Checks to see that Visual Studio Code is installed and working.'
   describe powershell('code --version') do
     its('stdout') { should match /1.31.1/ }
+  end
+end
+
+# https://hashicorp.github.io/se-terraform-vault-workshop/azure/terraform/#31
+control 'cd-desktop' do
+  impact 1.0
+  desc 'Change directory to the user desktop'
+  describe powershell(
+    'cd C:\Users\hashicorp\Desktop;
+    Get-Location'
+  ) do
+    its('stdout') { should match(/C:\\Users\\hashicorp\\Desktop/) }
+    its('stderr') { should match(//) }
+  end
+end
+
+# This must be in a separate control file from the training exercises. Otherwise
+# it will be using credentials from the *previous* test run.
+# https://hashicorp.github.io/se-terraform-vault-workshop/azure/terraform/#32
+control 'run-setup-script' do
+  impact 1.0
+  desc 'Run the setup.ps1 script'
+  describe powershell('powershell -ExecutionPolicy ByPass -File C:\Users\Public\Desktop\setup.ps1') do
+    its('stdout') { should match(/You may proceed with the workshop./) }
+    its('stderr') { should match(//) }
   end
 end
--- azure/test/inspec/training/controls/training.rb
+++ azure/test/inspec/training/controls/training.rb
@@ -2,29 +2,6 @@
 # Terraform Workshop Tests
 #################################################
 
-# https://hashicorp.github.io/se-terraform-vault-workshop/azure/terraform/#31
-control 'cd-desktop' do
-  impact 1.0
-  desc 'Change directory to the user desktop'
-  describe powershell(
-    'cd C:\Users\hashicorp\Desktop;
-    Get-Location'
-  ) do
-    its('stdout') { should match(/C:\\Users\\hashicorp\\Desktop/) }
-    its('stderr') { should match(//) }
-  end
-end
-
-# https://hashicorp.github.io/se-terraform-vault-workshop/azure/terraform/#32
-control 'run-setup-script' do
-  impact 1.0
-  desc 'Run the setup.ps1 script'
-  describe powershell('powershell -ExecutionPolicy ByPass -File C:\Users\Public\Desktop\setup.ps1') do
-    its('stdout') { should match(/You may proceed with the workshop./) }
-    its('stderr') { should match(//) }
-  end
-end
-
 # https://hashicorp.github.io/se-terraform-vault-workshop/azure/terraform/#38
 control 'git-clone' do
   impact 1.0
