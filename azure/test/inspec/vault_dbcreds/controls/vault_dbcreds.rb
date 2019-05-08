d094596c316267449ea050f3c5d7b2266d4ff6fc
Sean Carolan <scarolan@hashicorp.com>
2019-05-08 18:01:14 +0000

    Run inspec from the docker host.

--- azure/test/inspec/vault_dbcreds/controls/vault_dbcreds.rb
+++ azure/test/inspec/vault_dbcreds/controls/vault_dbcreds.rb
@@ -2,15 +2,16 @@
 # Vault DB Tests
 #################################################
 
-# Lab exercise 7a - Dynamic Creds - CLI
-control 'vault-fetch-dbcreds-cli' do
+# Lab exercise 7a - Dynamic Creds - API
+control 'vault-test-api-call' do
   impact 1.0
-  desc 'Fetch dynamic creds using the CLI'
-  describe powershell(
-    '$HOSTKEY=(ssh-keyscan -H uat-tf-vault-lab.centralus.cloudapp.azure.com | Select-String -Pattern "ed25519" | Select -ExpandProperty line);
-    plink.exe -ssh hashicorp@uat-tf-vault-lab.centralus.cloudapp.azure.com -pw Password123! -hostkey $HOSTKEY "VAULT_ADDR=http://127.0.0.1:8200 VAULT_TOKEN=root MYSQL_HOST=uat-tf-vault-lab-mysql-server vault read lob_a/workshop/database/creds/workshop-app"'
+  desc 'Fetch dynamic creds using the API'
+  describe http('http://uat-tf-vault-lab.centralus.cloudapp.azure.com:8200/v1/lob_a/workshop/database/creds/workshop-app-long', 
+    headers: {'X-Vault-Token' => 'root', 'Content-Type' => 'application/json'},
+    method: 'GET'
   ) do
-    its('stdout') { should match(/v-token-workshop-a/) }
+    its('status') { should be_in [200] }
+    its('body') { should match 'v-token-workshop-a' }
   end
 end
   
