d4ad763792811287f21a70d4d6429a06c97bee2e
Sean Carolan <scarolan@hashicorp.com>
2019-05-08 20:07:34 +0000

    Fix pip install command.

--- azure/files/transit_setup.sh
+++ azure/files/transit_setup.sh
@@ -18,7 +18,7 @@ vault write -f lob_a/workshop/transit/keys/archive-key
 # Install app prerequisites
 sudo apt-get -y update > /dev/null 2>&1
 sudo apt-get install -y python3-pip > /dev/null 2>&1
-sudo pip3 install mysql-connector-python hvac python3-flask > /dev/null 2>&1
+sudo -H pip3 install mysql-connector-python hvac flask > /dev/null 2>&1
 
 git clone https://github.com/norhe/transit-app-example.git
 
