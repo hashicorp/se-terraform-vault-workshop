fa53d84297deef11500b93df0d909c1029b23dcb
Sean Carolan <scarolan@hashicorp.com>
2019-06-16 17:09:07 -0500

    Fix regex for version match.

--- azure/test/inspec/terraform/controls/terraform.rb
+++ azure/test/inspec/terraform/controls/terraform.rb
@@ -71,7 +71,7 @@ control 'terraform-init' do
     terraform init'
   ) do
     its('exit_status') { should eq 0 }
-    its('stdout') { should match(/"\(1.30.1\)/) }
+    its('stdout') { should match(/1.30.1/) }
     its('stdout') { should match(/Terraform has been successfully initialized!/) }
     its('stderr') { should match(//) }
   end
