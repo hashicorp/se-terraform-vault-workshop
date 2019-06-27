f8f6ef7e2a9c167fddb33388368e651a0ef983bb
Andy James <ajames@hashicorp.com>
2019-06-26 23:14:59 -0500

    updated flies

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -8,20 +8,20 @@
 # providing instructions to the user on how to connect to their own custom
 # demo environment.
 #
-output "Vault_Server_URL" {
-  value = "http://${aws_instance.vault-server.public_ip}:8200"
-}
-output "MySQL_Server_FQDN" {
-  value = "${aws_db_instance.vault-demo.address}"
-}
-output "Instructions" {
-  value = <<EOF
-
-# Connect to your Linux Virtual Machine
+# output "Vault_Server_URL" {
+#   value = "http://${aws_instance.vault-server.public_ip}:8200"
+# }
+# output "MySQL_Server_FQDN" {
+#   value = "${aws_db_instance.vault-demo.address}"
+# }
+# output "Instructions" {
+#   value = <<EOF
 #
-# Run the command below to SSH into your server. You can also use PuTTY or any
-# other SSH client. Your SSH key is already loaded for you.
-
-ssh ubuntu@${aws_instance.vault-server.public_ip}
-EOF
-}
+# # Connect to your Linux Virtual Machine
+# #
+# # Run the command below to SSH into your server. You can also use PuTTY or any
+# # other SSH client. Your SSH key is already loaded for you.
+#
+# ssh ubuntu@${aws_instance.vault-server.public_ip}
+# EOF
+# }
